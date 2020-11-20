77d31f3c543522ae62f33f329f6164a3b37eb40d
marioffgallo <mario.felipe.gallo@hotmail.com>
2020-11-20 11:30:47 -0300

    Creating Services

--- src/main/java/com/github/project_Springboot_JPA/resources/UserResource.java
+++ src/main/java/com/github/project_Springboot_JPA/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.github.project_Springboot_JPA.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.project_Springboot_JPA.entities.User;
+import com.github.project_Springboot_JPA.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Joao", "joao@hotmail.com", "119876542", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
