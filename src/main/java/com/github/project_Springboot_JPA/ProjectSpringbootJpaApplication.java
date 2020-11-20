84b7aa1710fe54ac72528fea60d659c21d29ca21
marioffgallo <mario.felipe.gallo@hotmail.com>
2020-11-20 09:22:16 -0300

    Project created

---
+++ src/main/java/com/github/project_Springboot_JPA/ProjectSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.project_Springboot_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/project_Springboot_JPA/ProjectSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.project_Springboot_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
