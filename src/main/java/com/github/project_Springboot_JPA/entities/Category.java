1ed5fc4fe365d1090a470e432fb6b39c3d9e9b92
marioffgallo <mario.felipe.gallo@hotmail.com>
2020-11-25 18:58:11 -0300

    Many to Many connections update

--- src/main/java/com/github/project_Springboot_JPA/entities/Category.java
+++ src/main/java/com/github/project_Springboot_JPA/entities/Category.java
@@ -8,7 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -21,7 +24,8 @@ public class Category implements Serializable{
 	
 	private String name;
 	
-	@javax.persistence.Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
