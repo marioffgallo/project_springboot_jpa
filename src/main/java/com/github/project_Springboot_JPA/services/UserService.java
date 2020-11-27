700901ce2b55d85e02827c533aa9269d7880cd0a
marioffgallo <mario.felipe.gallo@hotmail.com>
2020-11-27 09:11:52 -0300

    ResourceException implmentado

--- src/main/java/com/github/project_Springboot_JPA/services/UserService.java
+++ src/main/java/com/github/project_Springboot_JPA/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.github.project_Springboot_JPA.entities.User;
 import com.github.project_Springboot_JPA.repositories.UserRepository;
+import com.github.project_Springboot_JPA.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
